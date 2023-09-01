618a325cec4c46ab2d559dafa6908fdd75935ee9
ZLOJ <jan.lutenko@gmail.com>
2023-09-01 14:04:55 +0200

    Add: main window layout with frames for each function

--- src/gui/windows/main_window.py
+++ src/gui/windows/main_window.py
@@ -6,7 +6,10 @@ import traceback
 
 from PyQt6.QtCore import QSize, Qt
 from PyQt6.QtGui import QAction
-from PyQt6.QtWidgets import QMainWindow, QToolBar
+from PyQt6.QtWidgets import (
+    QMainWindow, QToolBar, QVBoxLayout, QFrame,
+    QPushButton, QWidget
+)
 
 from .settings_window import SettingsWindow
 
@@ -32,6 +35,31 @@ class MainWindow(QMainWindow):
             settings_action.setStatusTip("Edit settings")
             settings_action.triggered.connect(self.get_settings_window)
             toolbar.addAction(settings_action)
+
+            central_widget = QWidget(self)
+            central_widget_layout = QVBoxLayout(central_widget)
+
+            for frame_name in ["Mod merge", "Mod manager"]:
+
+                frame = QFrame(self, objectName=frame_name)
+                layout = QVBoxLayout(frame)
+                test_button = QPushButton(frame_name, self)
+                layout.addWidget(test_button)
+                frame.setLayout(layout)
+                frame.hide()
+
+                frame_action = QAction(frame_name, self)
+                frame_action.setStatusTip(frame_name)
+                frame_action.triggered.connect(
+                    lambda checked, frame_name=frame_name: self.show_frame_by_name(frame_name)
+                )
+                toolbar.addAction(frame_action)
+
+                central_widget_layout.addWidget(frame)
+
+            central_widget.setLayout(central_widget_layout)
+            self.setCentralWidget(central_widget)
+
         except Exception as exc:
             traceback_formatted = traceback.format_exc()
             logger.info(traceback_formatted)
@@ -42,3 +70,13 @@ class MainWindow(QMainWindow):
         self.settings_window = SettingsWindow(self.settings)
         self.settings_window.setWindowModality(Qt.WindowModality.ApplicationModal)
         self.settings_window.show()
+
+    def show_frame_by_name(self, frame_name):
+        """ Takes a frame name as an input parameter. Searches main window for that frame and returns it """
+        frames_to_hide = self.findChildren(QFrame)
+        for frame in frames_to_hide:
+            frame.hide()
+        frame_to_show = self.findChild(QFrame, frame_name)
+        logger.debug(f"Found frame: {frame_to_show}")
+        if frame_to_show:
+            frame_to_show.show()
