e3057ad4e371f3c76e54b846d9ca4ba9d9cf8e83
ZLOJ <jan.lutenko@gmail.com>
2023-10-05 12:02:56 +0200

    Fix: ansi parsing

--- src/dbr_lib.py
+++ src/dbr_lib.py
@@ -1,18 +1,30 @@
 """ DBR operations library """
+import logging
 import os
+
+
+logger = logging.getLogger("tqma")
 
 
 def parse_dbr(dbr_file):
     """ Takes a file path as an input and parses it as a dbr. Returns dictionary with DBR data"""
+    logger.debug(f"Parsing {dbr_file}")
     dbr_data = {}
-    with open(dbr_file, 'r', encoding='utf-8') as dbr_contents:
-        for line in dbr_contents.readlines():
-            dbr_data[line.split(',', 1)[0]] = line.split(',', 1)[1].strip()
+    try:
+        with open(dbr_file, 'r', encoding='utf-8') as dbr_contents:
+            for line in dbr_contents.readlines():
+                dbr_data[line.split(',', 1)[0]] = line.split(',', 1)[1].strip()
+    except UnicodeDecodeError as err:
+        logger.debug(f"Couldn't parse with UTF-8, trying ansi")
+        with open(dbr_file, 'r', encoding='cp1252') as dbr_contents:
+            for line in dbr_contents.readlines():
+                dbr_data[line.split(',', 1)[0]] = line.split(',', 1)[1].strip()
 
     return dbr_data
 
 def write_dbr(dbr_data, output_file):
     """ Takes a dictionary with DBR data and writes it into an output file as a dbr """
+    logger.debug(f"Writing {output_file}")
     contents = ""
     for key, value in dbr_data.items():
         contents += key + "," + value + "\n"
